686f8b049b60e624a7978ab0c36bb762622b440c
Gregory Kutuzov <morgot.gregory@gmail.com>
2015-09-03 17:53:57 +0300

    add message api

--- net/http/model/property.go
+++ net/http/model/property.go
@@ -1,31 +1,32 @@
-// Copyright 2014 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+* Copyright 2015 Axibase Corporation or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+* https://www.axibase.com/atsd/axibase-apache-2.0.pdf
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+ */
 
 package model
 
 import (
 	"encoding/json"
-	"fmt"
+	netmodel "github.com/axibase/atsd-api-go/net/model"
+	"strings"
 )
 
 type Property struct {
-	propType     string
-	entity       string
-	key          map[string]string
-	tags         map[string]string
-	hasTimestamp bool
-	timestamp    uint64
+	propType  string
+	entity    string
+	key       map[string]string
+	tags      map[string]string
+	timestamp *netmodel.Millis
 }
 
 func NewProperty(propType, entity string) *Property {
@@ -45,12 +46,11 @@ func (self *Property) SetAllTags(tags map[string]string) *Property {
 	return self
 }
 func (self *Property) SetTag(name, value string) *Property {
-	self.tags[name] = value
+	self.tags[strings.ToLower(name)] = value
 	return self
 }
-func (self *Property) SetTimestamp(timestamp uint64) *Property {
-	self.hasTimestamp = true
-	self.timestamp = timestamp
+func (self *Property) SetTimestamp(timestamp netmodel.Millis) *Property {
+	self.timestamp = &timestamp
 	return self
 }
 func (self *Property) PropType() string {
@@ -73,33 +73,27 @@ func (self *Property) Tags() map[string]string {
 	}
 	return copy
 }
-func (self *Property) HasTimestamp() bool {
-	return self.hasTimestamp
+func (self *Property) TagValue(name string) (string, bool) {
+	val, ok := self.tags[strings.ToLower(name)]
+	return val, ok
 }
-func (self *Property) Timestamp() uint64 {
-	self.hasTimestamp = true
+func (self *Property) Timestamp() *netmodel.Millis {
 	return self.timestamp
 }
 func (self *Property) MarshalJSON() ([]byte, error) {
-	key, _ := json.Marshal(self.key)
-	tags, _ := json.Marshal(self.tags)
-	return []byte(fmt.Sprintf("{\"type\":\"%v\",\"entity\":\"%v\",\"key\":%v,\"tags\":%v,\"timestamp\":%v}", self.propType, self.entity, string(key), string(tags), self.timestamp)), nil
+	m := map[string]interface{}{
+		"key":      self.key,
+		"tags":     self.tags,
+		"type": self.propType,
+		"entity":   self.entity,
+	}
+	if self.timestamp != nil {
+		m["timestamp"] = *self.timestamp
+	}
+
+	return json.Marshal(m)
 }
 func (self *Property) String() string {
-	str := fmt.Sprintf("property e:%v t:%v", self.entity, self.propType)
-	if self.HasTimestamp() {
-		str += fmt.Sprintf(" ms:%v", self.timestamp)
-	}
-	keyString := ""
-	for key, val := range self.key {
-		keyString += fmt.Sprintf(" k:%v=%v", key, val)
-	}
-	str += keyString
-	tagsString := ""
-	for key, val := range self.tags {
-		tagsString += fmt.Sprintf(" v:%v=%v", key, val)
-	}
-	str += tagsString
-
-	return str
+	obj, _ := self.MarshalJSON()
+	return string(obj)
 }
--- net/http/model/series.go
+++ net/http/model/series.go
@@ -1,28 +1,57 @@
-// Copyright 2014 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+* Copyright 2015 Axibase Corporation or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+* https://www.axibase.com/atsd/axibase-apache-2.0.pdf
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+ */
 
 package model
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	netModel "github.com/axibase/atsd-api-go/net/model"
 	"strconv"
 	"strings"
 )
 
 type Sample struct {
-	T uint64  `json:"t"`
-	V float64 `json:"v"`
+	T netModel.Millis `json:"t"`
+	V netModel.Number `json:"v"`
+}
+
+func (self *Sample) UnmarshalJSON(data []byte) error {
+	var jsonMap map[string]interface{}
+	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.UseNumber()
+	if err := dec.Decode(&jsonMap); err != nil {
+		return err
+	}
+	self.T, _ = jsonMap["t"].(netModel.Millis)
+	fmt.Println(jsonMap)
+	switch value := jsonMap["v"].(type) {
+	case json.Number:
+		strRep := value.String()
+		if strings.Contains(strRep, ".") {
+			temp, _ := value.Float64()
+			self.V = netModel.Float64(temp)
+		} else {
+			temp, _ := value.Int64()
+			self.V = netModel.Int64(temp)
+		}
+	default:
+		panic(value)
+	}
+	return nil
 }
 
 type Series struct {
@@ -52,17 +81,17 @@ func (self *Series) MarshalJSON() ([]byte, error) {
 func (self *Series) TagValue(name string) string {
 	return self.Tags[name]
 }
-func (self *Series) AddTags(tags map[string]string) *Series {
+func (self *Series) SetTags(tags map[string]string) *Series {
 	for name, val := range tags {
-		self.Tags[name] = val
+		self.Tags[strings.ToLower(name)] = val
 	}
 	return self
 }
-func (self *Series) AddTag(name, value string) *Series {
+func (self *Series) SetTag(name, value string) *Series {
 	self.Tags[strings.ToLower(name)] = value
 	return self
 }
-func (self *Series) AddSample(t uint64, v float64) *Series {
+func (self *Series) AddSample(t netModel.Millis, v netModel.Number) *Series {
 	self.Data = append(self.Data, &Sample{T: t, V: v})
 	return self
 }
@@ -74,7 +103,7 @@ func (self *Series) String() string {
 	tags = tags + "}"
 	data := "["
 	for _, sample := range self.Data {
-		data = data + "{t: " + strconv.FormatUint(sample.T, 10) + ", v: " + strconv.FormatFloat(sample.V, 'f', -1, 64) + "}, "
+		data = data + "{t: " + strconv.FormatUint(uint64(sample.T), 10) + ", v: " + sample.V.String() + "}, "
 	}
 	data = data + "]"
 	return "{entity: \"" + self.Entity + "\", metric: \"" + self.Metric + "\", tags: " + tags + ", data: " + data + "}"
