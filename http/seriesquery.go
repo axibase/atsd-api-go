677c0b4326f8c35d49a25fe4d785be05553abccd
KutuzovGregory <morgot.gregory@gmail.com>
2017-02-28 17:19:59 +0300

    series query:  last parameter removed

--- http/seriesquery.go
+++ http/seriesquery.go
@@ -153,7 +153,6 @@ type SeriesQuery struct {
 	Limit        uint64              `json:"limit,omitempty"`
 	Entity       string              `json:"entity"`
 	Metric       string              `json:"metric"`
-	Last         bool                `json:"last,omitempty"`
 	Cache        bool                `json:"cache,omitempty"`
 	Type         SeriesType          `json:"type,omitempty"`
 	ForecastName string              `json:"forecastName,omitempty"`
